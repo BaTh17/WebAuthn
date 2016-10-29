5afd2785a7c401a3cb36ebffa9cf678a46fd1462
tschm23 <matscha7@gmail.com>
2016-10-29 11:24:24 +0200

    css + admin + utility

--- PHP/admin.php
+++ PHP/admin.php
@@ -1,25 +1,51 @@
-<html>
-<head>
-<title>admin.php</title>
-</head>
-<body>
 <?php 
-require_once('utility.php');
+//includes
 
+require_once('utility.php');
+require_once('util.php');
+
+// Session & Log
+session_start();
+$_SESSION['log'] = 'admin maske geladen';
+
+
+//definitions
+$pageTitle = 'admin.php';
+
+echo '<html>
+<head>
+<title>'.$pageTitle.'</title>
+		<link rel="stylesheet" href="../CSS/default.css" type="text/css">
+</head>
+<body>';
 echo '<p>admin.php</p>'; 
 echo '<p>Display and Change of policy and users</p>';
 
 
+
 $db = utility::dbconnect();
 
+$_SESSION['log'][] = 'erstes log erstellt';
 
+var_dump($_SESSION['log']);
 
 ?>
 
 <form action="utility.php" method="post" > 
-<input id=getUsersLabel value="" disabled=disabled /><input id=getUsers value="" /></br>
+<input id=getUsersLabel value="getUser" disabled=disabled /><input id=getUsers value="" /></br>
+<input id=setUser value="" disabled=disabled /><input id=getUsers value="" /></br>
 <input id=getUsersLabel value="" disabled=disabled /><input id=getUsers value="" /></br>
 </form>
+
+<div>
+<h1>Benutzertabelle</h1><input id=getUsers value="Anzeige neu laden" />
+<?php 
+
+createTable('WF_USER');
+
+?>
+
+</div>
 
  </body>
 </html>
--- PHP/utility.php
+++ PHP/utility.php
@@ -1,16 +1,93 @@
-<?php 
+<?php
 
 
 /**
  * Here are all functions for the serverside, that are used in different files of the thesis project.
- * If you want to enable these functions, use require_once('utility.php'); in the first lines of your php
- * script
- * @author Marcel
- *
- */
+* If you want to enable these functions, use require_once('utility.php'); in the first lines of your php
+* script
+* @author Marcel
+*/
 class utility {
+
+	
+
+	//get WF_USERs
+	/**
+	 * Get inforation about the given user
+	 */
+	function getWfUser($userInfo, $infoValue = 1){
+
+		$usersSQL = "SELECT * FROM WF_USER WHERE NAME = '$userInfo'";
+		$db = new $db;
+		$rs = $db->executeSQL($usersSQL);
+		if($rs){
+			return $rs;
+		}else{
+			return false;
+		}
+	}
+
+
+
+
+	//get Policy ( 0,1,2)
+
+	// create Policy (dropdown)
+
+
+	//get Keys from WF_USERS
+	//input ( string USERNAME)
+	// return True / false
+
+
+	// checkPassword (default)
+	//
+
+
+	//get Public KEy from USER (NAME)
+
+
+	//log in session
+
+
+	//create TAble
+
+	/**
+	 * erstellt eine Tabelle, param muss der Tabellenname sein
+	 */
+	function createTable($tableName)
+	{
+		$db = new db();
+		$sql = "SELECT * FROM $tableName";
+		$rs = $db->executeSQL();
+		$htmlOutput = '';
+		//Build HTML Table around result
+		if($rs){
+			$htmlOutput .= '
+				<table id=table_"'.$tableName.'" style="width:100%;border=1">
+					';
+			foreach($rs as $key => $value){
+				$htmlOutput .= '<tr>
+						<td id='.$key.'>'.$key.'</td><td>'.$value.'</td>
+					</tr>';
+			}
+
+
+			$htmlOutput .= '		</table>
+				';
+		}
+
+	}
+
+	
+
+}//end utility class
+
+
+class db{
 	
 	public $Datasource;
+	public $connection = NULL;
 	
 	/**
 	 * Constructor
@@ -67,120 +144,80 @@ class utility {
 		}
 	}
 	
-
-	public $connection = NULL;
 	
-function dbconnect(){
-//connect to mysql DB
-$host="localhost";
-$user="webflow";
-$password="1234";
-$database="thesis";
-$connection = mysqli_connect($host,$user,$password,$database);
-
-if ( $connection )
-{
-	echo 'Verbindung erfolgreich: ';
-	print_r( $connection);
-}
-else
-{
-	die('keine Verbindung möglich: ' . mysqli_error());
-}
-
-
-if ($db_link->connect_error) {
-	die("Connection failed: " . $connection->connect_error);
-}
-
-//set charset
-mysqli_set_charset($connection, 'utf8');
-
-$sql = "SELECT * FROM WF_USER";
-mysqli_query($connection, $sql) or die('Error selecting table WF_USER.');
-
-
-// Check connection
-if ($connection->connect_error) {
-	die("Connection failed: " . $db_link->connect_error);
-}
-
-return $connection;
-/*
- $host="localhost";
- $user="root";
- $password="";
- $con=mysql_connect($host,$user,$password);
- if(!$con) {
- echo '<h1>Connected to MySQL</h1>';
- //if connected then Select Database.
- $db=mysql_select_db("YOUR_DATABASE_NAME",$con);
- $query=mysql_query("YOUR_MYSQL_QUERY",$db);
- }
- else {
- echo '<h1>MySQL Server is not connected</h1>';
- }
- */
-}
-
-function executeSQL($sql, $asArray=false){	
-
-	mysqli_query($connection, $sql) or die('Error executing sql script');
 	
-
-	$result = $db_link->query($sql);
-	$result = $db_link->query($sql);
 	
-	if ($result->num_rows > 0) {
-		while($row = $result->fetch_assoc()) {
-			//echo "USERID: " . $row["USERID"]. " - NAME: " . $row["NAME"]. " - FULLNAME: " . $row["FULLNAME"]. "<br>";
-			$rs[] = $row;
+	function dbconnect(){
+		//connect to mysql DB
+		$host="localhost";
+		$user="webflow";
+		$password="1234";
+		$database="thesis";
+		$connection = mysqli_connect($host,$user,$password,$database);
+	
+		if ( $connection )
+		{
+			echo 'Verbindung erfolgreich: ';
+			print_r( $connection);
 		}
-	} else {
-		$rs = false;
-	}
-	$db_link->close();
-	return $rs;
-}
-
-//get WF_USERs
-/**
- * Get inforation about the given user
- */
-function getWfUser($userInfo, $infoValue = 1){
+		else
+		{
+			die('keine Verbindung möglich: ' . mysqli_error());
+		}
 	
-	$usersSQL = "SELECT * FROM WF_USER WHERE NAME = '$userInfo'";
-	$db = new $db;
-	$rs = $db->executeSQL($usersSQL);
-	if($rs){
-		return $rs;
-	}else{
-		return false;
+	
+		if ($db_link->connect_error) {
+			die("Connection failed: " . $connection->connect_error);
+		}
+	
+		//set charset
+		mysqli_set_charset($connection, 'utf8');
+	
+		$sql = "SELECT * FROM WF_USER";
+		mysqli_query($connection, $sql) or die('Error selecting table WF_USER.');
+	
+	
+		// Check connection
+		if ($connection->connect_error) {
+			die("Connection failed: " . $db_link->connect_error);
+		}
+	
+		return $connection;
+		/*
+		 $host="localhost";
+		 $user="root";
+		 $password="";
+		 $con=mysql_connect($host,$user,$password);
+		 if(!$con) {
+		 echo '<h1>Connected to MySQL</h1>';
+		 //if connected then Select Database.
+		 $db=mysql_select_db("YOUR_DATABASE_NAME",$con);
+		 $query=mysql_query("YOUR_MYSQL_QUERY",$db);
+		 }
+		 else {
+		 echo '<h1>MySQL Server is not connected</h1>';
+		 }
+		 */
 	}
-}
-
-
-
-//get Policy ( 0,1,2)
-
-// create Policy (dropdown)
-
-
-//get Keys from WF_USERS
-//input ( string USERNAME)
-// return True / false
-
-
-// checkPassword (default)
-//
-
-
-//get Public KEy from USER (NAME)
-
-
-
-}//end utility class
-
-class db{
+	
+	function executeSQL($sql, $asArray=false){
+	
+		mysqli_query($connection, $sql) or die('Error executing sql script');
+	
+	
+		$result = $db_link->query($sql);
+		$result = $db_link->query($sql);
+	
+		if ($result->num_rows > 0) {
+			while($row = $result->fetch_assoc()) {
+				//echo "USERID: " . $row["USERID"]. " - NAME: " . $row["NAME"]. " - FULLNAME: " . $row["FULLNAME"]. "<br>";
+				$rs[] = $row;
+			}
+		} else {
+			$rs = false;
+		}
+		$db_link->close();
+		return $rs;
+	}
 	
 }
