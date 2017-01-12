f4e85c8f1cc6a620ae90eb9880493bc7568e64d1
tschm23 <tschm23@users.noreply.github.com>
2017-01-12 10:33:05 +0100

    Update welcome.php

--- PHP/welcome.php
+++ PHP/welcome.php
@@ -1,50 +1,46 @@
 <?php 
 
-//redirection if Windows Hello is not active
 require_once('utility.php');
-utility::getConfiguration();
-$checkHelloIsActive = utility::getWindowsHelloStatus();
-//var_dump($checkHelloIsActive);
-if(!$checkHelloIsActive){
-//header("Location: http://localhost/phpmyadmin/");
-
-//$path_parts = pathinfo($_SERVER['PHP_SELF']);
-//$redirectUrl = $path_parts['dirname'].'/originalWebflowStartPage.php';
-
-//header("Location: http://localhost$redirectUrl");
-
-}
-//id="body-login"
 
 $pageTitle = 'Welcome - Page';
 echo '<!DOCTYPE html>
 <head>
-<title>'.$pageTitle.'</title>
-</head>
-<body>';
-
+<title>'.$pageTitle.'</title>';
 echo "<script src='../Client/utils.js'></script>";
 echo "<script src='../Client/webauthn.js'></script>";
 echo '<link rel="stylesheet" href="../CSS/default.css" type="text/css">';
+echo '</head>
+<body>';
 
 
 echo '<div class="titel" >'.$pageTitle.'</div>'; 
 echo '<div class="centerBox">';
-echo '<p class="heading" >Checking your operating system: </p>'; 
-//&#xDC;berpr&#xFC;fen der Systemumgebung:
-// kleines ue = &#xFC;  , grosses UE = &#xDC;
 
-echo '<span class="label">Operating System: </span>
+
+echo '<p class="heading topPadding" >Checking your operating system: </p>'; 
+//redirection if Windows Hello is not active
+$checkHelloIsActive = _plugin_utility::getWindowsHelloStatus();
+if(!$checkHelloIsActive){
+	$delayInSeconds = 5;
+	header( "refresh:$delayInSeconds;url=".$_SESSION['redirectToLogin']."" );
+	echo '<div class="label" style="width:600px;">Windows Hello Login is deactivated on this server. Redirect to login page in '.$delayInSeconds.' seconds...</div><br />';	
+}
+
+echo '<table class="systemInfoTable systemInfoLayout" >';
+echo '<tr class="systemInfoTable" style="width:180px;"><td class="systemInfoTable systemInfoLabel label">Operating System: </td>
+<td class="systemInfoTable systemInfoMessage">
 		<script>
 			if((navigator.appVersion.indexOf("Windows NT 10.0")) > -1) document.write("Windows 10");
 		</script>
-		<br />';
-echo "<span class='label'>Web Browser: </span>
-		<div id='browserInfo' class='' style='width:1200px;'></div>
+		</td>';
+echo "<tr class='systemInfoTable'>
+<td class='systemInfoTable systemInfoLabel label'>Web Browser:</td>
+		
+		
 ";
 
 echo "
-		
+	<td id='browserInfo' class='systemInfoTable systemInfoMessage'>	
 	<script>
 		if(window.navigator.userAgent.indexOf('Edge') > -1){
 			document.write('Microsoft Edge: Windows Hello login is possible.');
@@ -57,23 +53,23 @@ echo "
 		
 		
 	</script>
-		<br />
-						";
-// Verwendeter Browser ist Microsoft Edge.
-// Kein Edge, bitte Browser wechseln.
 
+		<br />
+	";
+
+echo '</td></table>';
+
+// Wrong browser, need microsoft edge
 echo '	
 		<img class="logo" alt="" src="../CSS/logo.png" >
 		<br />
 		<img class="icon" alt="" src="../CSS/user_white.png" >
-		Username: 
-		<input type="text" id="userNameInput" class="rounded" onKeydown="Javascript: if (event.keyCode == 13) login()"  autofocus />
+		Username: <input type="text" id="userNameInput" class="rounded defaultInput" onKeydown="Javascript: if (event.keyCode == 13) login()"  autofocus />
 		<br />
 		<br /><div><div class="label">Status message: </div><div id="status" class="message"></div></div>
 		<br />
 		<button id="loginButton" onclick="login()" class="rounded"><span><img class="" alt="" src="../CSS/Button_White_Check.png" ></span> Login</button>
 		';
-//<span><img class="logo" alt="" src="../CSS/Button_White_Check.png" ></span>
 
 echo '</div>';
 
