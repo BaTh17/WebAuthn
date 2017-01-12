78b0537b33f3520e3a2b7ee45290eb8cb9e0b13c
tschm23 <tschm23@users.noreply.github.com>
2017-01-12 10:04:49 +0100

    Update admin.php

--- PHP/admin.php
+++ PHP/admin.php
@@ -1,29 +1,18 @@
 <?php 
-
 /**
  * the administratior mask to view and regulate the policies
  * @autor MT
  */
-//includes
-if(!isset($_SESSION['log'])){
-	session_start();
-}
-
-require_once('utility.php');
+session_start();
 require_once('util.php');
+require_once('utility.php');
 
 if($_REQUEST['resetLog']=1){
-	utility::resetLog();
+	_plugin_utility::resetLog();
 }
 
-
 // Session & Log
-utility::addLog('Beginne mit Laden der Admin maske');
-
-
-utility::catchResponse($_REQUEST);
-
-//header("Location: ".$_SERVER['PHP_SELF'].'page=1');
+_plugin_utility::addLog('Beginne mit Laden der Admin maske');
 
 //definitions
 $pageTitle = 'Admin - Page';
@@ -32,96 +21,148 @@ echo '<!DOCTYPE html>
 <head>
 <title>'.$pageTitle.'</title>
 		<link rel="stylesheet" href="../CSS/default.css" type="text/css">
-		<script language="javascript" type="text/javascript">
+		
+		<script type="text/javascript">
 		
 		function createPolicy() 
 		{
-			var Varuserid = document.getElementById("select_WF_USER_USERID").value;
-			var Varpolicy = document.getElementById("select_PT_USER_USERID").value;
-			var VarStart = "createPolicy";
-		alert(Varuserid);
-		alert(Varpolicy);
-		//TODO TODO TODO
-		var getFullstring = VarStart.concat();
-		//alert(?createPolicy=1&userid=Varuserid&policyid=Varpolicy);
-		//id=select_'.$tableName.'_'.$id.'
-			window.location.href = "'.$_SERVER['PHP_SELF'].'?createPolicy=1&userid=Varuserid&policyid=Varpolicy";    
-		
+			var Varuserid = document.getElementById("select_WF_USER_USERID_FULLNAME").value;
+			var Varpolicy = document.getElementById("select_PT_USER_PTID_POLICY").value;
+			var Varfunction = "createPolicy";
+			//alert(Varuserid);
+			//alert(Varpolicy);
+			//alert(" " + Varuserid + " " + Varpolicy +" ");
+			window.location.href = "'.$_SERVER['PHP_SELF'].'?" + Varfunction + "=1&userid=" + Varuserid + "&policyid=" + Varpolicy +"";    
 		}
-					
+		
+		function deletePolicy()
+		{
+			var Varptid = document.getElementById("select_PT_USER_PTID_PTID").value;
+			var Varfunction = "deletePolicy";
+			window.location.href = "'.$_SERVER['PHP_SELF'].'?" + Varfunction + "=1&ptid=" + Varptid + "";    
+		}
+		
+		function deletePublicKey()
+		{
+			var Varkeyid = document.getElementById("select_PUBLICKEYS_KEYID_KEYID").value;
+			var Varfunction = "deletePublicKey";
+			window.location.href = "'.$_SERVER['PHP_SELF'].'?" + Varfunction + "=1&keyid=" + Varkeyid + "";    
+		}
+			
 		function changeWindowsHelloStatus()
 		{
-			//alert("calling me");
 			window.location.href = "'.$_SERVER['PHP_SELF'].'?changeWindowsHelloStatus=1";    
 		}
-					
-					
+
+		function reloadPage()
+		{
+			window.location.href = "'.$_SERVER['PHP_SELF'].'";    
+		}
 					
 		</script>
 </head>
 <body>';
-echo '<div class="titel">'.$pageTitle.'</div><br />';
+echo '<div class="titel">'.$pageTitle.' <input id="reloadPage" type="button" class="rounded button" value="reloadPage" onclick="return reloadPage()" /></div><br />';
 
+_plugin_utility::addLog('Admin mask called');
 
-$db = new db;
-utility::addLog('erste log-Meldung erstellt');
-
-
-//html Set Policy
+////////////////////////////
+// html Policy Table
+////////////////////////////
 echo '
-<h2>Benutzer Policy Tabelle (PT_USER)</h2>';
+<div class="heading">User Policy</div>';
 
-echo '		<span>Policy erstellen:</span>';
-utility::createSelect('WF_USER','USERID','FULLNAME');
-//utility::createSelect('PT_USER','USERID','POLICY');
-utility::createSelectPolicy('PT_USER','USERID','POLICY');
+echo '		<span>Create Policy: </span>';
+echo _plugin_utility::createSelect('WF_USER','USERID','FULLNAME');
+
+echo _plugin_utility::createSelectPolicy('PT_USER','PTID','POLICY');
 
 echo '
-		<input id="createPolicy" type="button" class="rounded button" value="createPolicy" onclick="return createPolicy()" />
+		<input id="createPolicy" type="button" class="rounded button" value="createPolicy" onclick="return createPolicy()" /><br />
+		';
+
+echo '		<span>Delete Policy, select PTID: </span>';
+echo _plugin_utility::createSelect('PT_USER','PTID','PTID');
+echo '
+		<input id="deletePolicy" type="button" class="rounded button" value="deletePolicy" onclick="return deletePolicy()" /><br />
 		';
 //loads the benutzertabelle
-print_r(utility::createTable('PT_USER'));
-
-
-//html Benutzertabelle
-echo '
-<div id="Benutzertabelle (WF_USER)">
-<div class="heading">Benutzertabelle</div><input id=getUsers value="Anzeige neu laden" />
-				<form action="utility.php" method="post" > 
-<input id=getUsersLabel value="getUser" disabled=disabled /><input id=getUsers value="" /><br />
-<input id=getUsersLabel value="setPolicy" disabled=disabled /><input id=getUsers value="" /><br />
-</form>
-		
-</div>
-';
+$result = _plugin_utility::createTable('PT_USER');
+if($result){
+	print_r($result);
+}else{
+	echo '  <div class="label"> No entries found!</div>';
+}
+echo '<br />';
 
 
 
-//loads the benutzertabelle
-print_r(utility::createTable('WF_USER'));
-//utility::addLog('WF_USER Table erstellt');
 
-
-echo '  <div class="heading">Settingstabelle</div>
+////////////////////////////
+// html Settings table
+////////////////////////////
+echo '  <div class="heading">Settings</div>
 		<span>Windows Hello Status:</span>
 		<input id="changeWindowsHelloStatus" type="button" class="rounded button" value="changeWindowsHelloStatus()" onclick="changeWindowsHelloStatus()" />
 
 		<p id="demo"></p>
 		
 		';
-//loads the benutzertabelle
-print_r(utility::createTable('SETTINGS'));
+$result = _plugin_utility::createTable('SETTINGS|WINDOWS_HELLO_STATUS');
+if($result){
+	print_r($result);
+}else{
+	echo '  <div class="label"> No entries found!</div>';
+}
+echo '<br />';
+
+////////////////////////////
+// html PublicKey Table
+////////////////////////////
+echo '
+<div id="PublicKey">
+<div class="heading">PublicKey</div>
+</div>
+';
+echo '<span>Delete PublicKey, select KEYID: </span>';
+echo _plugin_utility::createSelect('PUBLICKEYS','KEYID','KEYID');
+echo '
+		<input id="deletePublicKey" type="button" class="rounded button" value="deletePublicKey" onclick="return deletePublicKey()" /><br />
+		';
+//loads the User Table
+$result = _plugin_utility::createTable('PUBLICKEYS|KEYID,USERID,KEYVALUE,KEYIDENTIFIER,METADATA1,HOSTNAME,CREATEDTIME,CHANGEDTIME,AKTIV');
+if($result){
+	print_r($result);
+}else{
+	echo '  <div class="label"> No entries found!</div>';
+}
+echo '<br />';
+
+////////////////////////////
+// html User Table
+////////////////////////////
+echo '
+<div id="Users">
+<div class="heading">Application Users</div>
+</div>
+';
+
+//loads the User Table
+$result = _plugin_utility::createTable('WF_USER|USERID,NAME,FULLNAME,USERPASSWORD,AKTIV');
+if($result){
+	print_r($result);
+}else{
+	echo '  <div class="label"> No entries found!</div>';
+}
+echo '<br />';
 
 
-//test.php
-require_once('test.php');
-
-
-
-//print LogTabelle
+////////////////////////////
+// html Log Table
+////////////////////////////
 echo '<div id="logTable">';
-utility::addLog('WF_USER Table anzeigen');
-echo '<div class="heading">LOG</div>';
+_plugin_utility::addLog('WF_USER Table anzeigen');
+echo '<div class="heading">Log Messages</div>';
 if($_SESSION['log']){
 	echo '<table>';
 	foreach($_SESSION['log'] as $log){
@@ -134,6 +175,7 @@ if($_SESSION['log']){
 	echo 'no log found!';
 }
 echo '</div>';
+echo '<br />';
 
 
 echo '
