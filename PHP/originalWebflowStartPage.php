4c33159f695f5f9c46b9f8887088338d7c810aa7
tschm23 <tschm23@users.noreply.github.com>
2017-01-12 10:05:29 +0100

    Update originalWebflowStartPage.php

--- PHP/originalWebflowStartPage.php
+++ PHP/originalWebflowStartPage.php
@@ -1,28 +1,20 @@
 <?php
 /**
- * a dummy page to redirect to, so the flow is correct
+ * a dummy starting page of your application after the user successfully logged in.
+ * redirect to this local site, so the flow is correct. Has to be changed 
  * @autor MT
  */
 //includes
-if(!isset($_SESSION['log'])){
-	session_start();
-}
-
-require_once('utility.php');
+session_start();
 require_once('util.php');
 
 //definitions
-$pageTitle = 'Original Weblfow - Page';
+$pageTitle = 'Dummy Application Homepage';
 
 // Session & Log
-utility::addLog('Beginne mit Laden der '.$pageTitle.' - Maske');
+_plugin_utility::addLog('Beginne mit Laden der '.$pageTitle.' - Maske');
 
-
-utility::catchResponse($_REQUEST);
-
-//header("Location: ".$_SERVER['PHP_SELF'].'page=1');
-
-
+_plugin_utility::catchResponse($_REQUEST);
 
 echo '<!DOCTYPE html>
 <head>
@@ -31,10 +23,12 @@ echo '<!DOCTYPE html>
 		<script language="javascript" type="text/javascript"></script>
 </head>
 <body>';
-echo '<div class="title" >'.$pageTitle.'</div><br />';
-
+echo '<div class="titel" >'.$pageTitle.'</div><br />';
+echo '<p>This is a dummy site for the homepage of your application.</p>';
+echo '<p>The authentication of the user was valid.</p>';
+echo '<p>Here ends the Web Authentication API Demo.</p>';
 echo '
-<input class="button rounded" type="button" value="Back to Welcome" id="btnBackToWelcome"  onClick="document.location.href=\'welcome.php\'" />
+<input class="button rounded" type="button" value="Back to welcome page" id="btnBackToWelcome"  onClick="document.location.href=\'welcome.php\'" />
 		';
 
 ?>
