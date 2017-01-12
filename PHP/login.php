20ab6f5c7701e201604323647442e749beec5781
tschm23 <tschm23@users.noreply.github.com>
2017-01-12 10:30:24 +0100

    Update login.php

--- PHP/login.php
+++ PHP/login.php
@@ -4,19 +4,18 @@ require_once('util.php');
 session_start();
 
 
-getConfiguration();
 $username =  $_SESSION['username'];
 $policy =  $_SESSION['policy'];
 
 
 
 /*
- * Auf Keys gecheckt. wenn das true wäre, könnte man die Challenge schicken,
- * mit dem Risiko, dass keiner der ID's passt. Dann würde man JavaScript seitig die makeCredential Methode aufrufen.
+ * Check the keys, if it was true, the challenge can be send
+ * with the risk that none of the IDs will match. Then one would call the makeCredential mehtode from the client (javascript)
  * 
- * Wenn false:
- * 	1 (2FA): 	PW Eingabe und Meldung dass keine Keys + Button um Credentials zu generieren.
- * 	2 (HELLO):	Meldung dass keine Keys + Button um Credentials zu generieren
+ * if false:
+ * 	1 (2FA): 	password entry and a message that no keys are found and a button to create credentials
+ * 	2 (HELLO):	show a message that no keys are found + show button to generate credentials
  */
 $keys = $_SESSION['PKeys']; 
 
@@ -34,24 +33,24 @@ echo '<link rel="stylesheet" href="../CSS/default.css" type="text/css">';
 echo '<div class="titel" >'.$pageTitle.'</div>';
 echo '<div class="centerBox">';
 
-
-
 echo "
-		<div class='heading' >Known information:</div><br />
-		<div class='label float' style='clear:both;' >username:  </div>". $username." <br />
-		<div class='label float' >active policy: </div>".$policy."
-		<div class='clear'></div><br />
-		";
+		<p class='heading topPadding' >Known information:</p>
+		<table class='systemInfoTable systemInfoLayout'>
+		<tr tr class='systemInfoTable'><td class=' systemInfoTable systemInfoLabel label' >Username:</td><td class='systemInfoTable systemInfoMessage'>".$username."</td></tr>
+		<tr tr class='systemInfoTable'><td class=' systemInfoTable systemInfoLabel label' >Active policy:</td><td class='systemInfoTable systemInfoMessage'>".$policy."</td></tr>
+		</table>
+		<br />";
 
 /*
- * Je nach dem welche Policy für den Benutzer aktiv ist, wird der ReturnString zusammengebaut.
- * Bei 2 wird direkt die getAssertion Funktion aufgerufen
+ * Depending on the policy for the active user, the return-string is build
+ * policy calls getAssertion function directly
  */
 $pwCode = "
 <div>
 <img class='icon' alt='' src='../CSS/key_white.png' >
-Enter your password: <input class='rounded' type='text' id='pwInput' onKeydown='Javascript: if (event.keyCode == 13) checkPW()'  autofocus /><br />
-<button id='pwButton' class='rounded' onclick='checkPW()'>check password and proceed</button>
+Enter your password: <input class='rounded' type='text' id='pwInput' onKeydown='Javascript: if (event.keyCode == 13) checkPW(\"".$_SESSION['redirectToAfterSuccess']."\",\"".$policy."\")'  autofocus /><br />
+<br />
+<button id='pwButton' class='rounded' onclick='checkPW(\"".$_SESSION['redirectToAfterSuccess']."\",\"".$policy."\")'>check password and proceed</button>
 </div>
 ";
 
@@ -60,18 +59,15 @@ if($policy == 0 || $policy == 1) {
 	echo $pwCode;
 }
 
-//Entfernen: Denn hier kommt man gar nie rein
+//should never display but could be used if the order of checks is changed
 else {
 	echo "Passwordless is active. Call of getAssertion:<br />";
 	echo "<script>hello()</script>";
 	
 }
 
-
-echo "<p id='pwState'></p><br />
-	";
-echo "<br />
-		<p id='assertionState'></p>";
+echo "<p id='pwState' class='message' ></p>";
+echo "<p id='assertionState'  class='message'></p>";
 
 echo '</div>'; // from centerBox div
 
