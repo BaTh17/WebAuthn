859bc31b6f71afda2addd35282af5326c2f85e70
tschm23 <tschm23@users.noreply.github.com>
2017-01-12 10:00:15 +0100

    Update index.php

--- PHP/index.php
+++ PHP/index.php
@@ -1,29 +1,22 @@
 <?php 
 $pageTitle = 'Index - Page';
 echo '<!DOCTYPE html>
-<head>
-<title>'.$pageTitle.'</title>
+<head >
+ <title>'.$pageTitle.'</title>
+ <link rel="stylesheet" href="../CSS/default.css" type="text/css">
+</head>
 
-		</head>
-		<link rel="stylesheet" href="../CSS/default.css" type="text/css">
-
-		
-		<body>
-		';
-
-// <script language="javascript" type="text/javascript">
-
-
-echo '<div>'.$pageTitle.'</div>';
-echo '<p>Hello User! Please use the button below to go to the project start page.</p>'; 
+<body> 
+';
+echo '<div class="titel" >'.$pageTitle.'</div>'; 
+echo '<p>Hello user! Thank you for using our WebAuth prototyp.</p>';
+echo '<p>Please use the button below to go to the project start page.</p>'; 
 echo '
 				<button type="button" value="Back to Welcome" class="rounded" id="btnBackToWelcome"
-onClick="document.location.href=\'welcome.php\'" />
-		';
+onClick="document.location.href=\'welcome.php\'" >Go to welcome screen</button>
 
-
-
-
+';
+echo '<div ></div>';
 echo ' </body>
 </html>';
 
